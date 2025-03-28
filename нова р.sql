74046e2dca3f1d2785a9f773b9b07cb633364631
oleg6556456 <olegohrin337@gmail.com>
2025-03-28 12:28:59 +0200

    Add files via upload

---
+++ нова р.sql
@@ -0,0 +1,10 @@
+SELECT * FROM auction.enterprises;
+
+Insert into enterprises (cod_enterprises, names_company, address)
+Values (645, "magbook", "Petra street23"),
+(897, "apple", "Petra street23"),
+(900, "motorbycicle", "Petra street23"),
+(707, "skate", "Petra street23"),
+(534, "scooter", "Petra street23"),
+(767, "car", "Petra street23"),
+(876, "bicycle", "Petra street23");
